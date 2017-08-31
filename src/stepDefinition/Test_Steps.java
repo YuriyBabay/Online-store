626606a8dadcd58eb2aab916238a75101b42942c
YuriyBabay <31514828+YuriyBabay@users.noreply.github.com>
2017-09-01 00:49:45 +0300

    First commit

---
+++ src/stepDefinition/Test_Steps.java
@@ -0,0 +1,58 @@
+package stepDefinition;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import java.io.File;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+
+public class Test_Steps {
+	public static WebDriver driver ;
+	@Given("^User is on Home Page$")
+	public void user_is_on_Home_Page() throws Throwable {
+		File file = new File(".\\src\\drivers\\geckodriver.exe");
+		System.setProperty("webdriver.gecko.driver", file.getAbsolutePath());
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://www.store.demoqa.com");
+		}
+
+	@When("^User Navigate to LogIn Page$")
+	public void user_Navigate_to_LogIn_Page() throws Throwable {
+		driver.findElement(By.xpath(".//*[@id='account']/a")).click();
+		}
+
+	@When("^User enters UserName and Password$")
+	public void user_enters_UserName_and_Password() throws Throwable {
+		driver.findElement(By.id("log")).sendKeys("ybabay"); 	 
+	    driver.findElement(By.id("pwd")).sendKeys("CtdpAiDoz&TZJ8vc");
+	    driver.findElement(By.id("login")).click();
+		}
+
+	@Then("^Message displayed Login Successfully$")
+	public void message_displayed_Login_Successfully() throws Throwable {
+		System.out.println("Login Successfully");
+	}
+
+	@When("^User LogOut from the Application$")
+	public void user_LogOut_from_the_Application() throws Throwable {
+		driver.findElement (By.xpath(".//*[@id='account_logout']/a")).click();
+	}
+
+	@Then("^Message displayed LogOut Successfully$")
+	public void message_displayed_Logout_Successfully() throws Throwable {
+        System.out.println("LogOut Successfully");
+	}
+	@Then("^Close Browser$")
+	public void close_browser() throws Throwable {
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.quit();
+		System.out.println("Browser closed");
+	}
+}
